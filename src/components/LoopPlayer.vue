637978e55698a227daffff0404ae1a9f53e99ebd
Theo Hennessy <theo.hennessy@gmail.com>
2021-01-20 13:00:22 +0100

    Theo test

---
+++ src/components/LoopPlayer.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="loop-player">
+    <audio :class="playerViz" ref="audioLoop" controls loop :src="source"></audio>
+    <img @click="toggleMute" :class="{ muted: muted }" class="icon" :src="src"/>
+    <input type="range" min="0" max="1" step="0.2" v-model="volume" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoopPlayer',
+  props: {
+    source: { type: String, required: true },
+    name: { type: String, required: true }
+  },
+  data: function() {
+    return {
+      muted: false,
+      audio: null,
+      volume: 1,
+      playerViz: "hidden",
+    }
+  },
+  methods: {
+    playPause: function() {
+      if(this.audio.paused){
+        this.audio.play();
+        this.audio.currentTime = 0;
+      }
+      else {
+        this.audio.pause();
+      }
+    },
+    toggleMute: function() {
+      return this.muted = !this.muted;
+    }
+  },
+  computed: {
+    src: function(){
+      return this.name + ".png";
+    }
+  },
+  mounted: function() {
+    this.audio = this.$refs["audioLoop"];
+    console.log(this.audio)
+  },
+  watch:{
+    volume: function(value) {
+      this.audio.volume = value;
+    },
+    muted: function(value) {
+      this.audio.muted = value;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.loop-player {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem;
+}
+.hidden {
+  display: none;
+}
+.icon {
+  transition: all 0.5s ease;
+  &.muted {
+    filter: grayscale(100%);
+    opacity: 0.5;
+  }
+  width: 96px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Jazz-en-Boucle</h1>
+    <button @click="playPause">{{ label }}</button>
+    <div class="players">
+      <LoopPlayer ref="son1" name="sax" source="son1.wav"></LoopPlayer>
+      <LoopPlayer ref="son2" name="keys" source="son2.wav"></LoopPlayer>
+      <LoopPlayer ref="son3" name="bass" source="son3.wav"></LoopPlayer>
+      <LoopPlayer ref="son4" name="drums" source="son4.wav"></LoopPlayer>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LoopPlayer from '@/components/LoopPlayer.vue'
 
 export default {
   name: 'Home',
+  data: function() {
+    return {
+      playing: false,
+      label: "Démarrer"
+    }
+  },
+  methods: {
+    playPause: function() {
+      if (!this.playing){
+        this.label = "Arrêter";
+      }
+      else {
+        this.label = "Démarrer";
+      }
+      this.$refs["son1"].playPause();
+      this.$refs["son2"].playPause();
+      this.$refs["son3"].playPause();
+      this.$refs["son4"].playPause();
+      this.playing = !this.playing;
+    }
+  },
   components: {
-    HelloWorld
+    LoopPlayer
+  }
+};
+</script>
+
+<style lang="scss">
+body, html {
+  margin: 0;
+  padding: 0;
+  top: 0;
+}
+@font-face {
+  font-family: "Berkah";
+  src: url("~@/assets/fonts/Berkah.ttf") format("truetype");
+}
+h1 {
+  font-family: "Berkah";
+  text-align: center;
+  color: white;
+  font-size: 64px;
+  margin: 1rem;
+}
+button {
+  display: inline-block;
+  margin: 1rem auto;
+  width: 100%;
+  font-size: 1.4rem;
+  text-align: center;
+}
+.home {
+  max-width: 480px;
+  margin: 0 auto;
+  .players {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
   }
 }
-</script>
+</style>
